f74961ae248e0c9beecde174582a2d24daba9e1e
rans-hekel <ransenju27@gmail.com>
2021-10-04 22:36:37 +0700

    tugas pertemuan 4

---
+++ application/views/v_about.php
@@ -0,0 +1,21 @@
+<h4>Nama</h4>
+<ul type="disc">
+    <li>Nama Depan : El Rangga</li>
+    <li>Nama Belakang : Un Cahya</li>
+</ul>
+<br>
+<h4>Alamat</h4>
+<ul type="none">
+    <li> jakpus</li>
+</ul>
+
+<h4>Tempat Lahir</h4>
+<ul type="none">
+    <li>lampung</li>
+</ul>
+<h4>Olah Raga Favorit</h4>
+<ul type="square">
+    <li>sepakbola</li>
+    <li>Catur</li>
+</ul>
+</section>
